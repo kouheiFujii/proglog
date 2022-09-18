f12de14b99b265a09ed8a09d53f4644688c17587
kouheifujii <kohei.fujii@i-nations.tokyo>
2022-09-18 16:30:23 +0900

    create store

--- internal/log/store.go
+++ internal/log/store.go
@@ -44,9 +44,11 @@ func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	pos = s.size
+	// バイナリをio.Writerに書き込む
 	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
 		return 0, 0, err
 	}
+	// システムコールを減らすためにバッファ付きWriterに書き込む
 	w, err := s.buf.Write(p)
 	if err != nil {
 		return 0, 0, err
@@ -54,4 +56,43 @@ func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
 	w += lenWidth
 	s.size += uint64(w)
 	return uint64(w), pos, nil
+}
+
+// 引数に渡ってきた位置に格納されているレコードを返す
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+	b := make([]byte, enc.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+// storeのFileのoffオフセット　からlen(p)バイトをpへ読み込む
+func (s *store) ReadAt(p []byte, off int64) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+	return s.File.ReadAt(p, off)
+}
+
+// バッファされたデータを永続化する
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	err := s.buf.Flush()
+	if err != nil {
+		return err
+	}
+	return s.File.Close()
 }
